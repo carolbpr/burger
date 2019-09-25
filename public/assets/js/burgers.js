291e1dae97a215ed41ecb6136b73214b50d666b2
Caro Berry <carolbpr@gmail.com>
2019-09-25 15:06:05 -0500

    running perfect

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,56 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("devoured");
+    console.log(id);
+    console.log(newDevoured);
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed Devoured", newDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    
+    var newBurger = {
+      burger_name: $("#bu").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event){
+    const id = $(this).data("id");
+
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(function(){
+      console.log("delete burger: " + id);
+      location.reload();
+    })
+  })
+});
